d018d208bf0920a2693b364d4356c455f0812b08
jaybhuptani10 <115783493+jaybhuptani10@users.noreply.github.com>
2023-09-19 19:44:40 +0530

    new

--- script.js
+++ script.js
@@ -7,16 +7,16 @@
 submitbtn.addEventListener("click",async(e)=>{
     e.preventDefault()
     resultcont.innerHTML = `<img width="133" src="loader.svg" alt="">`
-    var key = "ema_live_Fu4bILHTfy9C5wc7c2MBeRd9gkSTBu6arOgdQ3Vm"
-    var email = document.getElementById("username").value
+    let key = "ema_live_Fu4bILHTfy9C5wc7c2MBeRd9gkSTBu6arOgdQ3Vm"
+    let email = document.getElementById("username").value
     
     let url =`https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
     let res = await fetch(url)
     let result = await res.json()
     let str =``
-    for(key of Object.keys(results)){
+    for(key of Object.keys(result)){
         if (result[key] !=="" && result[key]!==" " ){
-            str += `<div> ${key} : ${results[key]} </div>  `
+            str += `<div> ${key} : ${result[key]} </div>  `
         }
        
     }
@@ -24,20 +24,20 @@ submitbtn.addEventListener("click",async(e)=>{
     resultcont.innerHTML = str
 })
 
-let results ={
-    "tag": "",
-    "free": true,
-    "role": false,
-    "user": "jaybhuptani1054",
-    "email": "jaybhuptani1054@gmail.com",
-    "score": 0.64,
-    "state": "deliverable",
-    "domain": "gmail.com",
-    "reason": "valid_mailbox",
-    "mx_found": true,
-    "catch_all": null,
-    "disposable": false,
-    "smtp_check": true,
-    "did_you_mean": "",
-    "format_valid": true
-  }
+// let results ={
+//     "tag": "",
+//     "free": true,
+//     "role": false,
+//     "user": "jaybhuptani1054",
+//     "email": "jaybhuptani1054@gmail.com",
+//     "score": 0.64,
+//     "state": "deliverable",
+//     "domain": "gmail.com",
+//     "reason": "valid_mailbox",
+//     "mx_found": true,
+//     "catch_all": null,
+//     "disposable": false,
+//     "smtp_check": true,
+//     "did_you_mean": "",
+//     "format_valid": true
+//   }
